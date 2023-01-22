8be1c7b82d52ed71bda8b3949100421a61dfd63f
dmartin305 <dmartin305@gatech.edu>
2023-01-22 15:40:20 -0500

    refactoring

--- contact.js
+++ contact.js
@@ -1,96 +1,59 @@
 import context from "./context";
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import gsap from "gsap";
+import anim from "./animation";
 
+var linkedinButton, emailButton, linkedin, email, rotation, position;
 let icons = context.icons.contact;
-let frame = 0;
-const linkedinURL = "https://www.linkedin.com/in/david-martin-507132148";
-let header = document.getElementById("david-martin");
-let buttonGroup = document.getElementById("button-group");
-let topnav = document.getElementById("topnav");
-let backButton = document.getElementById("back-button");
-let cameraX = window.innerWidth < 768 ? -120 : -100;
+let tl = gsap.timeline();
+
 export default {
   start() {
-    let rotation = context.camera.rotation;
-    let position = context.camera.position;
-    if (!context.pagesVisited.includes("contact")) {
-      this.loadObjects();
-      context.pagesVisited.push("contact");
-    }
     context.world.currentPage = "contact";
-    let tl = gsap.timeline();
+    let unVisited = !context.pagesVisited.includes("contact");
+    if (unVisited) {
+      context.isMobile ? this.loadHTML() : this.loadObjects();
+      context.pagesVisited.push("contact");
+      rotation = context.camera.rotation;
+      position = context.camera.position;
+      email = context.isMobile ? emailButton : icons.email.material;
+      linkedin = context.isMobile ? linkedinButton : icons.linkedin.material;
+    }
 
-    tl.to(header, {
-      x: -innerWidth / 2 + header.clientWidth / 2,
-      y: -innerHeight / 2,
-      opacity: 0,
-      duration: 1,
-    })
-      .to(
-        buttonGroup,
-        {
-          x: innerWidth / 2 - buttonGroup.clientWidth / 2,
-          y: -innerHeight / 2,
-          opacity: 0,
-          duration: 1,
-        },
-        "-=1"
-      )
-      .to(rotation, {
-        z: -Math.PI / 2,
-        y: -Math.PI / 2,
-        duration: 1,
-        ease: "power4.in",
-      })
-      .to(
-        position,
-        {
-          z: 10,
-          x: cameraX,
-          duration: 1,
-          ease: "power1.in",
-        },
-        "-=1"
-      )
-      .to(
-        document.getElementById("gui-container"),
-        {
-          opacity: 0,
-          duration: 1,
-          ease: "power4.out",
-        },
-        "-=1"
-      )
-      .to(
-        document.getElementById("contact-container"),
-        {
-          opacity: 1,
-          zIndex: 1,
-          duration: 1,
-          ease: "power4.in",
-        },
-        "-=.5"
-      )
-      .to(icons.linkedin.material, {
-        opacity: 1,
-        duration: 1,
-      })
-      .to(icons.email.material, {
-        opacity: 1,
-        duration: 1,
-      })
-      .to(document.getElementById("back-button"), {
-        opacity: 1,
-        duration: 0.5,
-      });
+    tl.to(context.header, anim.panOut(context.header))
+      .to(context.buttonGroup, anim.panOut(context.buttonGroup), "-=1")
+      .to(rotation, anim.contactRotation)
+      .to(position, anim.contactLocation, "-=1")
+      .to(context.mainMenu, anim.hideHTML, "-=1")
+      .to(context.mobileMenu, anim.revealHTML)
+      .to(linkedin, anim.revealMesh)
+      .to(email, anim.revealMesh)
+      .to(context.backButton, anim.revealHTML);
+  },
+  loadHTML() {
+    linkedinButton = document.createElement("a");
+    linkedinButton.id = "linkedin-button";
+    linkedinButton.className = "link";
+    linkedinButton.innerHTML =
+      '<button class = "david-text button-35">Visit my Linkedin</button>';
+    document
+      .getElementById("mobile-button-container")
+      .appendChild(linkedinButton);
+    emailButton = document.createElement("a");
+    emailButton.id = "email-button";
+    emailButton.className = "link";
+    emailButton.innerHTML =
+      '<button class = "david-text button-35">Copy my Email</button>';
+    document.getElementById("mobile-button-container").appendChild(emailButton);
+
+    linkedinButton.addEventListener("click", this.onClickLinkedin);
+    emailButton.addEventListener("click", this.onClickEmail);
   },
   loadObjects() {
     const texture = new THREE.TextureLoader();
     const geometry = new THREE.PlaneGeometry(15, 15);
     const material = new THREE.MeshBasicMaterial({
       map: texture.load("../logos/linkedinlogo.png"),
-
       side: THREE.DoubleSide,
       opacity: 0,
     });
@@ -116,124 +79,50 @@ export default {
     this.returnHome();
   },
   returnHome() {
-    let tl = gsap.timeline();
-
-    tl.to(icons.email.material, {
-      opacity: 0,
-      duration: 1,
-    })
-      .to(
-        icons.linkedin.material,
-        {
-          opacity: 0,
-          duration: 1,
-        },
-        "-=1"
-      )
-      .to(
-        document.getElementById("back-button"),
-        {
-          opacity: 0,
-          duration: 0.5,
-        },
-        "-=1"
-      )
-      .to(
-        document.getElementById("contact-container"),
-        {
-          opacity: 0,
-          zIndex: -1,
-          duration: 1,
-          ease: "power4.out",
-        },
-        "-=.5"
-      )
-      .to(
-        document.getElementById("gui-container"),
-        {
-          opacity: 1,
-          duration: 1,
-          ease: "power4.in",
-        },
-        "-=1"
-      )
-      .to(
-        header,
-        {
-          x: -innerWidth / 2 + header.innerWidth / 2,
-          y: innerHeight / 2 - header.innerHeight / 2,
-          opacity: 1,
-          duration: 1,
-        },
-        "-=.5"
-      )
-      .to(
-        buttonGroup,
-        {
-          x: -innerWidth / 2 + buttonGroup.innerWidth / 2,
-          y: innerHeight / 2 - buttonGroup.innerHeight / 2,
-          opacity: 1,
-          duration: 1,
-        },
-        "-=1"
-      )
-      .to(context.camera.rotation, {
-        z: 0,
-        y: 0,
-        x: 0,
-        duration: 1,
-        ease: "power4.out ",
-      })
-      .to(
-        context.camera.position,
-        {
-          x: 0,
-          y: 0,
-          z: 100,
-          duration: 1,
-          ease: "power1.out",
-        },
-        "-=1"
-      );
     context.world.currentPage = "home";
+    tl.to(email, context.isMobile ? anim.hideHTML : anim.hideMesh)
+      .to(linkedin, context.isMobile ? anim.hideHTML : anim.hideMesh, "-=1")
+      .to(context.backButton, anim.hideHTML, "-=1")
+      .to(context.mainMenu, anim.revealHTML, "-=1")
+      .to(context.header, anim.panIn(context.header), "-=.5")
+      .to(context.buttonGroup, anim.panIn(context.buttonGroup), "-=1")
+      .to(rotation, anim.mainMenuRotation)
+      .to(position, anim.mainMenuLocation, "-=1");
   },
   animate() {
-    const intersectsLinkedin =
-      context.raycaster.intersectObject(icons.linkedin).length > 0;
-    const intersectsEmail =
-      context.raycaster.intersectObject(icons.email).length > 0;
-    if (intersectsLinkedin) {
+    if (this.intersects(icons.linkedin)) {
       icons.linkedin.scale.x = 1.2;
       icons.linkedin.scale.y = 1.2;
       context.world.contact.linkedinhover = true;
     } else {
-      context.world.contact.linkedinhover = false;
       icons.linkedin.scale.x = 1;
       icons.linkedin.scale.y = 1;
-      icons.linkedin.rotation.y += Math.cos((frame * 1) / 2) * 0.001;
-      // linkedinLogo.position.z += Math.cos(frame + Math.random()) * 0.05;
+      icons.linkedin.rotation.y += 0.002;
+      context.world.contact.linkedinhover = false;
     }
 
-    if (intersectsEmail) {
+    if (this.intersects(icons.email)) {
       icons.email.scale.x = 1.2;
       icons.email.scale.y = 1.2;
       context.world.contact.emailhover = true;
     } else {
-      context.world.contact.emailhover = false;
       icons.email.scale.x = 1;
       icons.email.scale.y = 1;
-      icons.email.rotation.y -= Math.cos((frame * 1) / 2) * 0.002;
-      // email.position.z -= Math.cos(frame + Math.random()) * 0.03;
+      icons.email.rotation.y += 0.002;
+      context.world.contact.emailhover = false;
     }
-    if (context.world.contact.linkedinhover) {
-      document.getElementById("contact-helper-text").innerText =
-        "Click to visit my Linkedin Page";
-    } else if (context.world.contact.emailhover) {
-      document.getElementById("contact-helper-text").innerText =
-        "Click to copy my email address";
-    } else {
-      document.getElementById("contact-helper-text").innerText = "";
-    }
+    // if (context.world.contact.linkedinhover) {
+    //   document.getElementById("helper-text").innerText =
+    //     "Click to visit my Linkedin Page";
+    // } else if (context.world.contact.emailhover) {
+    //   document.getElementById("helper-text").innerText =
+    //     "Click to copy my email address";
+    // } else {
+    //   document.getElementById("helper-text").innerText = "";
+    // }
+  },
+  intersects(object) {
+    return context.raycaster.intersectObject(object).length > 0;
   },
   onClick() {
     this.onClickLinkedin();
@@ -242,14 +131,14 @@ export default {
   onClickLinkedin() {
     const linkedinHover = context.world.contact.linkedinhover;
     if (linkedinHover) {
-      window.open(linkedinURL);
+      window.open(context.linkedinURL);
       context.world.contact.linkedinhover = false;
     }
   },
   async onClickEmail() {
     // TODO add feedback for user to know email is copied
     const emailHover = context.world.contact.emailhover;
-    if (emailHover) {
+    if (emailHover || context.isMobile) {
       try {
         await navigator.clipboard.writeText("dmartin305@gatech.edu");
         console.log("Content copied to clipboard");
