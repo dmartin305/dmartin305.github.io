e79606a467ba5c9bdb7c349b6b5a966aef2fd6ec
dmartin305 <dmartin305@gatech.edu>
2023-01-22 16:38:56 -0500

    refactoring and improvements to readability

--- about.js
+++ about.js
@@ -1,133 +1,46 @@
 import context from "./context";
 import gsap from "gsap";
-let header = document.getElementById("david-martin");
-let buttonGroup = document.getElementById("button-group");
-let backButton = document.getElementById("back-button");
+import anim from "./animation";
+
+var rotation, position;
+// let context.buttonGroup = document.getElementById("button-group");
+// let context.backButton = document.getElementById("back-button");
+let tl = gsap.timeline();
 export default {
   start() {
-    let rotation = context.camera.rotation;
-    let position = context.camera.position;
-
-    if (!context.pagesVisited.includes("about")) {
-      this.loadObjects();
-      context.pagesVisited.push("about");
-    }
     context.world.currentPage = "about";
-    let tl = gsap.timeline();
-
-    tl.to(header, {
-      x: -innerWidth / 2 + header.clientWidth / 2,
-      y: -innerHeight / 2,
-      opacity: 0,
-      duration: 1,
-    })
-      .to(
-        buttonGroup,
-        {
-          x: innerWidth / 2 - buttonGroup.clientWidth / 2,
-          y: -innerHeight / 2,
-          opacity: 0,
-          duration: 1,
-        },
-        "-=1"
-      )
-      .to(rotation, {
-        x: Math.PI / 2,
-        duration: 1,
-        ease: "power4.in",
-      })
-      .to(
-        position,
-        {
-          z: 10,
-          y: 100,
-          duration: 1,
-          ease: "power1.in",
-        },
-        "-=1"
-      )
-      .to(
-        document.getElementById("gui-container"),
-        {
-          opacity: 0,
-          duration: 1,
-          ease: "power4.out",
-        },
-        "-=1"
-      )
-      .to(backButton, {
-        opacity: 1,
-        duration: 0.5,
-      });
+    let unVisited = !context.pagesVisited.includes("contact");
+    if (unVisited) {
+      if (context.isMobile) {
+        this.loadHTML();
+      } else {
+        this.loadObjects();
+      }
+      context.pagesVisited.push("about");
+      rotation = context.camera.rotation;
+      position = context.camera.position;
+    }
+    tl.to(context.header, anim.panOut(context.header))
+      .to(context.buttonGroup, anim.panOut(context.buttonGroup), "-=1")
+      .to(rotation, anim.aboutRotation)
+      .to(position, anim.aboutLocation, "-=1")
+      .to(context.mainMenu, anim.hideHTML, "-=1")
+      .to(context.mobileMenu, anim.revealHTML)
+      .to(context.backButton, anim.revealHTML);
   },
+  loadHTML() {},
   loadObjects() {},
   return() {
     this.returnHome();
   },
   returnHome() {
-    let tl = gsap.timeline();
-    tl.to(
-      document.getElementById("back-button"),
-      {
-        opacity: 0,
-        duration: 0.5,
-      },
-      "-=1"
-    )
-      .to(
-        document.getElementById("contact-container"),
-        {
-          opacity: 0,
-          zIndex: -1,
-          duration: 1,
-          ease: "power4.out",
-        },
-        "-=.5"
-      )
-      .to(document.getElementById("gui-container"), {
-        opacity: 1,
-        duration: 0,
-      })
-      .to(topnav, { height: 0, opacity: 0, duration: 1 })
-      .to(
-        header,
-        {
-          x: -innerWidth / 2 + header.innerWidth / 2,
-          y: innerHeight / 2 - header.innerHeight / 2,
-          opacity: 1,
-          duration: 1,
-        },
-        "-=.5"
-      )
-      .to(
-        buttonGroup,
-        {
-          x: -innerWidth / 2 + buttonGroup.innerWidth / 2,
-          y: innerHeight / 2 - buttonGroup.innerHeight / 2,
-          opacity: 1,
-          duration: 1,
-        },
-        "-=1"
-      )
-      .to(context.camera.rotation, {
-        z: 0,
-        y: 0,
-        x: 0,
-        duration: 1,
-        ease: "power4.out ",
-      })
-      .to(
-        context.camera.position,
-        {
-          x: 0,
-          y: 0,
-          z: 100,
-          duration: 1,
-          ease: "power1.out",
-        },
-        "-=1"
-      );
     context.world.currentPage = "home";
+    tl.to(context.backButton, anim.hideHTML, "-=1")
+      .to(context.mainMenu, anim.revealHTML, "-=1")
+      .to(context.header, anim.panIn(context.header), "-=.5")
+      .to(context.buttonGroup, anim.panIn(context.buttonGroup), "-=1")
+      .to(rotation, anim.mainMenuRotation)
+      .to(position, anim.mainMenuLocation, "-=1");
   },
   animate() {},
   onClick() {},
