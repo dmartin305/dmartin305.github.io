da4b0bcb13693ce6574099d5837c958f0b6a3d53
dmartin305 <dmartin305@gatech.edu>
2023-01-22 16:31:36 -0500

    add animations for about page

--- animation.js
+++ animation.js
@@ -1,25 +1,24 @@
 let halfPI = Math.PI / 2;
 
 export default {
-  
-    // CAMERA LOCATIONS
-  contactLocation: { z: 10, x: -100, duration: 1, ease: "power1.in" },
+  // CAMERA LOCATIONS
+  contactLocation: { x: -100, y: 0, z: 10, duration: 1, ease: "power1.in" },
   resumeLocation: {},
-  aboutLocation: {},
+  aboutLocation: { x: 0, y: 100, z: 10, duration: 1, ease: "power1.in" },
   mainMenuLocation: { x: 0, y: 0, z: 100, duration: 1, ease: "power4.out" },
- 
+
   // CAMERA ROTATION
   contactRotation: { y: -halfPI, z: -halfPI, duration: 1, ease: "power4.in" },
   resumeRotation: {},
-  aboutRotation: {},
+  aboutRotation: { x: halfPI, y: 0, z: 0, duration: 1, ease: "power4.out" },
   mainMenuRotation: { x: 0, y: 0, z: 0, duration: 1, ease: "power4.out" },
- 
+
   // VISIBILITY
   hideHTML: { opacity: 0, zIndex: -1, duration: 1, ease: "power4.out" },
   revealHTML: { opacity: 1, zIndex: 1, duration: 0.25 },
   hideMesh: { opacity: 0, duration: 1, ease: "power4.out" },
   revealMesh: { opacity: 1, duration: 0.25 },
-  
+
   // MAIN MENU PAN IN/OUT
   panOut: function (element) {
     return {
